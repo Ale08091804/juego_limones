52b0149030209191643b532c4f7ccf657a261c46
Ale08091804 <valeria.mosquera.1804@gmail.com>
2026-07-07 19:58:49 -0500

    mover derecha

--- limones.js
+++ limones.js
@@ -5,6 +5,8 @@ const ALTURA_SUELO=20;
 const Altura_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 
+let personajeX=canvas.width/2;
+
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
@@ -16,5 +18,25 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+Altura_PERSONAJE),ANCHO_PERSONAJE,Altura_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+Altura_PERSONAJE),ANCHO_PERSONAJE,Altura_PERSONAJE);
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+}
+
+function moverDerecha(){
+    personajeX=personajeX +10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
